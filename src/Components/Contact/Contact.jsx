ae2a12e2b30b1836fbbd79430fc1457218a25580
rishav8755 <91119048+rishav8755@users.noreply.github.com>
2024-05-10 17:46:23 +0530

    9899887

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
       const response = await fetch("https://api.web3forms.com/submit", {
         method: "POST",
         body: formData
-      }).then((res) => res.json());
+      }).then((response) => response.json());
   
   
       if (response.success) {
